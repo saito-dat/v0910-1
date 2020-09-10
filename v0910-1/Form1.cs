64411e5fe0db7848bc76f96fe993207b25b3ed86
Fukata <satoshidat@gmail.com>
2020-09-10 09:29:37 +0900

    1段階目

--- v0910-1/Form1.cs
+++ v0910-1/Form1.cs
@@ -12,9 +12,32 @@ namespace v0910_1
 {
     public partial class Form1 : Form
     {
+        private static Random rand = new Random();
+        int[] vx = new int[3];
+        int[] vy = new int[3];
+        Label[] labels = new Label[3];
+
+
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 3; i++)
+            {
+                vx[i] = rand.Next(-15, 16);
+                vy[i] = rand.Next(-15, 16);
+
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                Controls.Add(labels[i]);
+
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
